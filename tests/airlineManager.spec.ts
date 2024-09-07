5cd397872dca47e911595871b21588a11b01c704
Mahmud Hassan <59851214+Mahmudhassan18@users.noreply.github.com>
2024-09-07 12:23:29 +0300

    Update airlineManager.spec.ts

--- tests/airlineManager.spec.ts
+++ tests/airlineManager.spec.ts
@@ -59,17 +59,5 @@ test('All Operations', async ({ page }) => {
   await fleetUtils.departPlanes();
   // End //
 
-  // Fuel Operations //
-  await page.locator('#mapMaint > img').first().click();
-  
-  await fuelUtils.buyFuel();
-
-  await page.getByRole('button', { name: ' Co2' }).click();
-  await GeneralUtils.sleep(1000);
-  await fuelUtils.buyCo2();
-
-  await page.locator('#popup > .modal-dialog > .modal-content > .modal-header > div > .glyphicons').click();
-  // End //
-
   page.close();
 });
